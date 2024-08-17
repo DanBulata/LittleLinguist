43af96f41ff2db779bc89a8fe81712fd949bfc0c
DanBulata <ono.bait2@gmail.com>
2024-08-17 13:25:42 +0300

    win-lose-dialog

---
+++ src/app/win-lose-dialog/win-lose-dialog.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WinLoseDialogComponent } from './win-lose-dialog.component';
+
+describe('WinLoseDialogComponent', () => {
+  let component: WinLoseDialogComponent;
+  let fixture: ComponentFixture<WinLoseDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [WinLoseDialogComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(WinLoseDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/word-sorter/word-sorter.component.ts
+++ src/app/word-sorter/word-sorter.component.ts
@@ -3,11 +3,17 @@ import { Category } from '../../shared/model/category';
 import { CategoriesService } from '../services/categories.service';
 import { ExitGameComponent } from "../exit-game/exit-game.component";
 import { CoinsComponent } from "../coins/coins.component";
+import { ExitDialogComponent } from '../exit-dialog/exit-dialog.component';
+import { WinLoseDialogComponent } from '../win-lose-dialog/win-lose-dialog.component';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-word-sorter',
   standalone: true,
-  imports: [ExitGameComponent, CoinsComponent],
+  imports: [ExitGameComponent, CoinsComponent, MatIconModule, CommonModule, MatDialogModule],
   templateUrl: './word-sorter.component.html',
   styleUrl: './word-sorter.component.css'
 })
@@ -16,10 +22,18 @@ export class WordSorterComponent implements OnInit {
   @Input()
   id = "";
   currentCategory?: Category;
-  constructor(private categoriesService: CategoriesService) { }
+  isSuccess: boolean | undefined;
+  // isSuccess=true
+  constructor(private categoriesService: CategoriesService, private dialog: MatDialog) { }
 
 
   ngOnInit(): void {
     this.currentCategory = this.categoriesService.get(parseInt(this.id));
   }
+
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(WinLoseDialogComponent, {data: { isSuccess: this.isSuccess }})
+  }
+
 }
