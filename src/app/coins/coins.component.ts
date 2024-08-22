4347e9f1c48c8cb687e0c6aa34ec0c195e0751d6
BarLongo <barlongo70@gmail.com>
2024-08-22 23:05:00 +0300

    coins in html and score visualization on result page

--- src/app/coins/coins.component.ts
+++ src/app/coins/coins.component.ts
@@ -1,14 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { CoinsService } from '../services/coins.service';
 
 @Component({
   selector: 'app-coins',
   standalone: true,
-  imports: [MatIconModule,CommonModule],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './coins.component.html',
   styleUrl: './coins.component.css'
 })
 export class CoinsComponent {
 
+  constructor(private coinsService: CoinsService) {}
+
+  getCoins() {
+    return this.coinsService.get()
+  }
 }
