be206fd15cf15b7d612e3e874584f84458d9eef6
BarLongo <barlongo70@gmail.com>
2024-08-25 21:38:48 +0300

    Mixed letters game

--- src/app/mixed-letters/mixed-letters.component.ts
+++ src/app/mixed-letters/mixed-letters.component.ts
@@ -5,14 +5,30 @@ import { ExitGameComponent } from "../exit-game/exit-game.component";
 import { CoinsComponent } from "../coins/coins.component";
 import { TranslatedWord } from '../../shared/model/translated-word';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
-import { MatInputModule } from '@angular/material/input';
+import { MatInput, MatInputModule } from '@angular/material/input';
+import { ResultRow, ResultsTableComponent } from '../results-table/results-table/results-table.component';
+import { ProgressBarComponent } from '../progress-bar/progress-bar/progress-bar.component';
+import { FormsModule } from '@angular/forms';
+import { WinLoseDialogComponent } from '../win-lose-dialog/win-lose-dialog.component';
+import { CoinsService } from '../services/coins.service';
 
 @Component({
   selector: 'app-mixed-letters',
   standalone: true,
-  imports: [ExitGameComponent, CoinsComponent, MatFormFieldModule, MatDialogModule,CommonModule,MatInputModule],
+  imports: [
+    ExitGameComponent, 
+    CoinsComponent, 
+    MatFormFieldModule, 
+    MatDialogModule, 
+    CommonModule, 
+    MatInputModule, 
+    ResultsTableComponent, 
+    ProgressBarComponent,
+    FormsModule,
+    WinLoseDialogComponent
+  ],
   templateUrl: './mixed-letters.component.html',
   styleUrl: './mixed-letters.component.css'
 })
@@ -20,31 +36,93 @@ export class MixedLettersComponent implements OnInit {
   @Input()
   id = "";
   currentCategory?: Category;
-  words? :TranslatedWord[];
-  index = -1;
-  mixedWord: string = "";
-chosenCategoryWords: any;
-  constructor(private categoriesService: CategoriesService) { }
+  grade: number = 0
 
+  currentWordIndex: number = 0
+  words: TranslatedWord[] = []
+  scrabledWords: string[] = []
+  results: ResultRow[] = []
+  guesses: boolean[] = []
+
+  constructor(private categoriesService: CategoriesService, private dialog: MatDialog, private coinsService: CoinsService) { }
 
   ngOnInit(): void {
-    this.currentCategory = this.categoriesService.get(parseInt(this.id));
-    let chosenCategoryWords: TranslatedWord[] = this.currentCategory?.words || [];
-    
+    this.initGame()
   }
 
+  initGame(): void {
+    this.grade = 0
+    this.currentWordIndex = 0
+    this.guesses = []
+    this.results = []
+    this.scrabledWords = []
 
+    this.currentCategory = this.categoriesService.get(parseInt(this.id));
+    let chosenCategoryWords: TranslatedWord[] = this.currentCategory?.words || [];
 
+    const wordsMaxLength = chosenCategoryWords.length > 10 ? 10 : chosenCategoryWords.length
+    const randomwords: TranslatedWord[] = []
+    for (let i = 0; i < wordsMaxLength; i++) {
+      // כדי לא לבחור באותה מילה פעמיים, יש להוציא את המילה מהאפשרויות
+      const wordIndex = Math.floor(Math.random() * (chosenCategoryWords.length))
+      const word = chosenCategoryWords[wordIndex]
+      randomwords.push({ ...word, origin: word.origin.toLowerCase() })
+      chosenCategoryWords = chosenCategoryWords.slice(0, wordIndex).concat(chosenCategoryWords.slice(wordIndex + 1))
+    }
 
+    this.words = randomwords
 
+    for (const word of this.words) {
+      this.scrabledWords.push(this.scramble(word.origin))
+    }
 
+    console.log("words: ", this.words)
+    console.log("s: ", this.scrabledWords)
+  }
 
+  reset() {
+    this.initGame()
+  }
 
+  scramble(word: string): string {
+    const array = word.split('');
 
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
 
+    return array.join('');
+  }
 
+  submit(): void {
+    const currentWord = this.words[this.currentWordIndex]
+    const gussedCorrectly = currentWord.origin === currentWord.guess
+    this.guesses.push(gussedCorrectly)
+    const dialogRef = this.dialog.open(WinLoseDialogComponent, { data: { isSuccess: gussedCorrectly } })
 
+    dialogRef.afterClosed().subscribe(() => {
+      this.currentWordIndex++
 
+      if (gussedCorrectly) {
+        this.grade += Math.floor(100 / this.words.length)
+      }
+
+      if (this.currentWordIndex === this.words.length) {
+        this.coinsService.set(this.coinsService.get() + this.grade)
+
+        for (let i = 0; i < this.words.length; i++) {
+          this.results = this.results.concat({
+            correct: this.guesses[i],
+            origin: this.words[i].origin,
+            target: this.words[i].target
+          })
+        }
+      }
+    })
+  }
+
+  resetInput(): void {
+    this.words[this.currentWordIndex].guess = ""
+  }
 }
-
-  
