36ca1419520a25f71189854b528a7a248912673e
BarLongo <barlongo70@gmail.com>
2024-09-23 18:43:59 +0300

    fromFirestore converter

--- src/app/services/converters/category-converter.ts
+++ src/app/services/converters/category-converter.ts
@@ -1,39 +1,41 @@
 import { QueryDocumentSnapshot,  SnapshotOptions, Timestamp } from '@angular/fire/firestore';
 import { Category } from '../../../shared/model/category';
+import { TranslatedWord } from '../../../shared/model/translated-word';
 // import { Language } from '../../../shared/model/language';
-
 export const categoryConverter = {
   toFirestore: (categoryToSave: Category) => {
-    const words = [];
+    const wordsArr = [];
     for (let i = 0; i < categoryToSave.words.length; i++) {
-      words.push({
+      wordsArr.push({
         origin: categoryToSave.words[i].origin,
         target: categoryToSave.words[i].target,
       });
     }
-
     return {
       name: categoryToSave.name,
       origin: categoryToSave.origin,
       target: categoryToSave.target,
-      lastUpdateDate: Timestamp.fromDate(categoryToSave.lastUpdateDate).toDate(),  //toDate??
+      lastUpdateDate: Timestamp.fromDate(categoryToSave.lastUpdateDate).toDate(),
+      words: wordsArr   
     };
   },
-
-
-  fromFirestore: (
+fromFirestore: (
     snapshot: QueryDocumentSnapshot,
     options: SnapshotOptions
   ) => {
     const data = snapshot.data(options)
-
     const category = new Category(
       snapshot.id,
       data['name'],        
       data['origin'],
       data['target']
     )
-
+    const words = data['words'];
+  if (words) {
+    for (let i = 0; i < words.length; ++i) {
+      category.words.push(new TranslatedWord(words[i].origin, words[i].target));
+    }
+  }
     category.lastUpdateDate = data['lastUpdateDate'].toDate()
     return category
   }
